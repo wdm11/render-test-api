5d04f07682543dd2d9257cabaee4d4825a1a48ab
wdm11 <wdmeyer11@gmail.com>
2025-12-21 11:04:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+import os
+import requests
 
 app = FastAPI()
 
 @app.get("/ping")
 def ping():
     return {"status": "ok"}
+
+
+LEAGUES = {
+    "NFL": "americanfootball_nfl",
+    "NCAAF": "americanfootball_ncaaf",
+    "NCAAB": "basketball_ncaab",
+    "NHL": "icehockey_nhl"
+}
+
+@app.get("/games")
+def get_games(league: str):
+    return {
+        "games": [
+            {"id": "test1", "away": "Oklahoma", "home": "Alabama"},
+            {"id": "test2", "away": "Texas", "home": "Georgia"}
+        ]
+    }
